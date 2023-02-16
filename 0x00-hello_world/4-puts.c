76dbb113c278e1b6a1b4567732ba53727d9aa02a
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-02-16 08:11:35 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program is to use the put funtion
+ * Return: 0
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+};
