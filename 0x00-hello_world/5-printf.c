51671f074d12028cccdb2f9abd19c50cdc20437a
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-02-16 09:39:47 -0800

    printf new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - this project requires rhe use of printf function
+ * main - This project requies the use of printf function
  * Return: 0
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
-}
+)
