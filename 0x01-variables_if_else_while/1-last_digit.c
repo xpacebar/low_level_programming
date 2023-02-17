74b03ea567b739e0b9a8e1904c24f5c1920d6b8e
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-02-17 11:24:46 -0800

    last digit new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l_digit = (n % 10);
+	int l_digit = (n % 10);
 	if (l_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
 	else if (l_digit == 0)
