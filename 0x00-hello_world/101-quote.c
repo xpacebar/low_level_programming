844f8973ba83217866a5d767671f2906a92c1e6f
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-02-16 23:55:01 -0800

    new quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,7 @@
  * main - to print a script followed by a new line to std error
  * Return: 1
  */
+
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
