2f4b704ed0134cc9d46c82f01195252993cbd7ca
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-02-17 13:01:08 -0800

    lower upper alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints lower case aplhabets follower by uppercase alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+	for (alp = 'A'; alp <= 'Z'; alp++)
+		putchar(alp);
+	putchar('\n');
+	return (0);
+}
