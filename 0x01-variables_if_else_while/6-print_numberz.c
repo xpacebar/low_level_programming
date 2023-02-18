b1b51fda5f0b7eebb778a3ea1b652bebc19f79aa
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-02-18 12:16:34 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints num from xero to nine
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
