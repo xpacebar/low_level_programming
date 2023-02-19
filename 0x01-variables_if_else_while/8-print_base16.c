d77715a66b5ac6065ff17beef72786f0c9862675
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-02-19 06:19:10 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - to print all numbers of base 16 in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	int h;
+	char low;
+
+	for (h = '1'; h <= '9'; h++)
+		putchar(h);
+
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
