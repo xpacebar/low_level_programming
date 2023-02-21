b2651c4c332eac2ea7227b2d6c3be4c68d9ae182
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-02-21 08:00:59 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
