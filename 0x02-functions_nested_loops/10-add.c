77312839af847890e4b0bc739725859efa00ec65
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-02-21 15:08:17 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function thatadds 2 numbers and return the result
+ * @a1: first number to compute
+ * @a2: second number to compute
+ * Return: integer value
+ */
+
+int add(int a1, int a2)
+{
+
+	return (a1 + a2);
+}
