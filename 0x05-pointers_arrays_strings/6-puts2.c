55c24fa0f4c3b52d1cc734fba75bb995c7f261a1
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-02-27 07:44:27 -0800

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,7 @@
 void puts2(char *str)
 {
 	int longi = 0;
-	int i = 0;
+	int t = 0;
 	char *y = str;
 	int o;
 
@@ -19,7 +19,7 @@ void puts2(char *str)
 		longi++;
 	}
 	t = longi - 1;
-	for (0 = 0; o <= t; o++)
+	for (o = 0; o <= t; o++)
 	{
 		if (o % 2 == 0)
 		{
