1d6b0290a373d86657af45afb50785f803010d2f
Opeoluwa Akinuliola <opeoluwa007@gmail.com>
2023-03-23 03:14:03 -0700

    3-functions.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,13 +21,13 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
-	
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0)){
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
 		printf("Error\n");
 		exit(100);
 	}
