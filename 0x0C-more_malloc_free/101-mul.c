66c0b48406d221250c20d7a913a0a998721a7246
xpacebar <opeoluwa007@gmail.com>
2023-03-29 17:11:45 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -7,18 +8,14 @@
  * @argc: n arguments
  * @argv: args
  * Return: int
- */
+**/
 
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
 	unsigned long mul;
 	int i, j;
 
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
 	{
 		printf("Error\n");
 		exit(98);
@@ -29,17 +26,13 @@ int i, j;
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
 			{
 				printf("Error\n");
 				exit(98);
 			}
 		}
-
 	}
 	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
-return (0);
 	return (0);
 }
