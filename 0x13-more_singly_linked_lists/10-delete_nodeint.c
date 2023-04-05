2a85baecc531c75690907b70b4e21ac3c345b679
xpacebar <opeoluwa007@gmail.com>
2023-04-04 17:33:00 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 
-	currentt = temp->next;
+	current = temp->next;
 	temp->next = current->next;
 	free(current);
 	return (1);
