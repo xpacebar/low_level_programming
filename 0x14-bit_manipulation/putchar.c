f59af9d277d2f62a4698f0b21d873fd86a82b0cc
xpacebar <opeoluwa007@gmail.com>
2023-05-04 04:57:52 -0700

    _putchar

---
+++ 0x14-bit_manipulation/putchar.c
@@ -0,0 +1,13 @@
+#include <unitstd.hg>
+#include "main.h"
+
+/**
+ * _putchar - Entry point
+ * @c: character to write
+ * Return: 0. (always success)
+ */
+
+int _putchar(char c)
+{
+	write (1, &c, 1);
+}
