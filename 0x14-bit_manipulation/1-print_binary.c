4fe138b9ef7189c01a727106a14522bdf8ca3e9e
xpacebar <opeoluwa007@gmail.com>
2023-05-04 06:35:15 -0700

    updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,9 +3,7 @@
 /**
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
- * Return: 0
  */
-
 void print_binary(unsigned long int n)
 {
 	int i, count = 0;
@@ -23,6 +21,6 @@ void print_binary(unsigned long int n)
 		else if (count)
 			_putchar('0');
 	}
-	if (count)
+	if (!count)
 		_putchar('0');
 }
