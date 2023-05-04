3ef4b4acfb392c7e4a44724d5c08d088eccdc182
xpacebar <opeoluwa007@gmail.com>
2023-05-04 07:51:49 -0700

    updated

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - function to set bit to index
+ * @n: poibnter to the number to change
+ * @index: index of the bit to set to 1
+ * Return: 1 (for success); -1 for failure
+ */
+
+int set_bit(unsigned long int *n, unsigned long int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
