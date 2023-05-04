4a3fd0a2fcfe433b308299793e1a6527e8b7be36
xpacebar <opeoluwa007@gmail.com>
2023-05-04 07:15:06 -0700

    updated

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,9 +4,9 @@
  * get_bit - returns the value of a bit at an index in a decimal number
  * @n: number to search
  * @index: index of the bit
- *
  * Return: value of the bit
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit_val;
