e5ec145bdf2395531ba8f811ce9b913b5c6bc45c
xpacebar <opeoluwa007@gmail.com>
2023-08-22 07:07:07 -0700

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -Pendantic -c -fPIC *.c
+gcc -Wall -Wextra -Werror -pendantic -c -fPIC *.c
 gcc  *.o -shared -o liball.so
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
